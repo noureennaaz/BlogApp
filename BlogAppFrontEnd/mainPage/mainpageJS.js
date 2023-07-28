effdf00832d1def7d2ac4d8d056d938b42dacb00
noureennaaz <noureennaaz09@gmail.com>
2023-07-28 15:33:15 +0530

    separated frontEnd

--- BlogAppFrontEnd/mainPage/mainpageJS.js
+++ BlogAppFrontEnd/mainPage/mainpageJS.js
@@ -139,4 +139,75 @@ const Likeicon=document.querySelector('[like-icon]');
         }
 
 
+
   
+
+// rendering the info
+const ArticleContainer=document.querySelector(".Article-Container");
+console.log(ArticleContainer)
+
+function renderBlogPosts(posts) {
+    
+    console.log(1);
+    
+    
+    // console.log(ArticleContainer);
+    posts.forEach(function(post) {
+        
+      const person=document.createElement("div").classList.add("writerinfo")
+      console.log(1);
+      var AllText=document.createElement("div").classList.add("textContent").appendChild(person)
+      var container=document.createElement("div").classList.add("upperPart").appendChild(AllText);
+      var Elementcontainer2=document.createElement("div").classList.add("article-body").appendChild(container);
+      var Elementcontainer1 = document.createElement('div').classList.add('article').appendChild(Elementcontainer2);
+      
+      console.log(AllText);
+      
+    //   Elementcontainer2.classList.add("article-body");
+
+
+    
+      
+    //   var titleElement = document.createElement('h2');
+    //   titleElement.textContent = post.title;
+  
+    //   var contentElement = document.createElement('p');
+    //   contentElement.textContent = post.content;
+  
+    //   var dateElement = document.createElement('span');
+    //   dateElement.textContent = post.date;
+  
+    //   postElement.appendChild(titleElement);
+    //   postElement.appendChild(contentElement);
+    //   postElement.appendChild(dateElement);
+  
+    //   blogContainer.appendChild(postElement);
+    });
+  }
+
+
+
+
+
+
+
+const getPost=async()=>{
+    try{
+        const response=await fetch(`${process.env.BASE_URL}/posts`);
+        console.log("a")
+        const Allposts = await response.json();
+        // console.log(Allposts?.posts[0]);
+
+        const title=[];
+        const postText=[];
+        var i=0 ;
+
+        renderBlogPosts(Allposts);
+    
+
+    }
+    catch{
+        console.log("fetch failed");
+    }
+}
+getPost();
